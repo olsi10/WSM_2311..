3f64c16527a33e4b661cbe6802b704e4ecd57fab
olsi10 <w2127@e-mirim.hs.kr>
2022-06-15 11:09:13 +0900

    function 여러가지 사용법

---
+++ 20220608_school cafeteria/js/main.js
@@ -0,0 +1,11 @@
+// const say = () =>  { console.log("Hello, world3"); }
+
+// const say = function () {
+//     console.log("Hello, world2");
+// }
+
+// function say() {
+//     console.log("Hello, world1");
+// }
+
+// say();
