ea4473cab76d78f36716ef59856ac0b7cc87a7f2
olsi10 <w2127@e-mirim.hs.kr>
2022-06-15 11:43:34 +0900

    JS toggle menu()

--- 20220608_school cafeteria/js/main.js
+++ 20220608_school cafeteria/js/main.js
@@ -1,3 +1,33 @@
+//show / hide menu <-> 토글 메뉴(껐다 켰다 반복)
+
+function toggleMenu (toggleID, navListId) {
+    //html -> js
+    const toggle = document.getElementById(toggleID);
+    const navList = document.getElementById(navListId);
+    console.log(toggle, navList);
+
+    function clickHandler() {
+        //밖에서 호출할 수 없는 함수 
+        navList.classList.toggle('show-menu');
+    }
+
+    if (toggle && navList) {
+        //toggle click
+        toggle.addEventListener('click', clickHandler);
+        
+        //show/hide menu : show-menu 있으면 height : 0 없으면 164px
+    
+    }
+
+
+
+}
+
+toggleMenu('nav-toggle', 'nav-list');
+
+
+//=====================================
+
 // const say = () =>  { console.log("Hello, world3"); }
 
 // const say = function () {
