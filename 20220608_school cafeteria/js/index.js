7eff0f17e1309eaf9f419dbcb4c922471bfe0563
olsi10 <wangbampang2035@gmail.com>
2022-10-31 12:14:56 +0900

    신기술 비동기 호출 -> AJAX

--- 20220608_school cafeteria/js/index.js
+++ 20220608_school cafeteria/js/index.js
@@ -57,3 +57,39 @@ const addNow = (mainCardId) => {
 }
 
 addNow('main-card');
+
+// 급식 멘ㅈ뉴ㅜ 표시하장~~~~
+const showTodayMenu = () => {
+    // 년, 월, 일 구하기
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let date = now.getDate();
+
+    // 급식 API url 만들어서 구하고 year, month, date 정보 가져와서 url 생성
+    const KEY = "42a1d43c85f848fdaed1ad5a7b5325ab";
+    const ATPT_OFCDC_SC_CODE = "B10";           // 서울특별시교육청
+    const SD_SCHUL_CODE = "7010569";            // 미림여자정보과학고
+    let MLSV_YMD = `${year}${month.toString().padStart(2, "0")}${date.toString().padStart(2, "0")}`;    // 2022 / 10 / 19
+    // 한자리의 경우 앞자리에 0을 넣어야 함 ex) 3일 -> 03
+    // let MMEAL_SC_CODE = 2;                   // 중식2 석식3 조식1
+
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
+
+    url += `?KEY=${KEY}`;
+    url += `&Type=json`;
+    url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
+    url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
+    url += `&MLSV_YMD=${MLSV_YMD}`;
+    // url += `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`;
+
+    console.log(url);
+
+    // 비동기로 호출
+    fetch(url).then((response) => response.json()).then((json) => console.log(json)); // 신기술 비동기 호출 -> AJAX
+    // = 응답이 오면 문자열인 것을 json으로 형변환해라
+
+    // 응답오면 표시
+}
+
+showTodayMenu();
